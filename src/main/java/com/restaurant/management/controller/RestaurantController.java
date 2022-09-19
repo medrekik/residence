a74a0ccf71727c89a8323997bdc2c6c29fcd48f4
medrekik <mohammed.rekik@stud.enis.tn0>
2022-09-19 14:43:16 +0200

    add cors origin

--- src/main/java/com/restaurant/management/controller/RestaurantController.java
+++ src/main/java/com/restaurant/management/controller/RestaurantController.java
@@ -1,7 +1,8 @@
 package com.restaurant.management.controller;
 
 import java.util.List;  
-import org.springframework.beans.factory.annotation.Autowired;  
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;  
 import org.springframework.web.bind.annotation.GetMapping;  
 import org.springframework.web.bind.annotation.PathVariable;  
@@ -14,7 +15,7 @@ import com.restaurant.management.model.Restaurant;
 import com.restaurant.management.service.RestaurantService;
 
 @RestController  
-
+@CrossOrigin(origins = "*")
 public class RestaurantController {
 	//autowire the BooksService class  
 	@Autowired  
